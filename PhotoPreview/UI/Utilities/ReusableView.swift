7dafabfbda827ba287b7589cdc9723f77723d293
Tomasz Pietrowski <tomasz.pietrowski@tooploox.com>
2018-02-26 00:06:59 +0100

    Added UICollectionViewCell registering conveniences.

---
+++ PhotoPreview/UI/Utilities/ReusableView.swift
@@ -0,0 +1,21 @@
+//
+//  ReusableView.swift
+//  PhotoPreview
+//
+//  Created by Tomasz Pietrowski on 25.02.2018.
+//  Copyright © 2018 Tomasz Pietrowski. All rights reserved.
+//
+
+import UIKit
+
+protocol ReusableView: class {
+    static var defaultReuseIdentifier: String { get }
+}
+
+extension ReusableView where Self: UIView {
+    
+    static var defaultReuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
---
+++ PhotoPreview/UI/Utilities/UICollectionView+registering.swift
@@ -0,0 +1,26 @@
+//
+//  UICollectionView+registering.swift
+//  PhotoPreview
+//
+//  Created by Tomasz Pietrowski on 25.02.2018.
+//  Copyright © 2018 Tomasz Pietrowski. All rights reserved.
+//
+
+import UIKit
+
+extension UICollectionReusableView: ReusableView {}
+
+extension UICollectionView {
+    
+    func register<T: UICollectionViewCell>(_: T.Type) {
+        register(T.self, forCellWithReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.defaultReuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
+        }
+        
+        return cell
+    }
+}
