99ac4579bba3b267fed14ad5d206d9f5fe363390
Tomasz Pietrowski <tomasz.pietrowski@icloud.com>
2018-09-27 11:22:09 +0200

    Add use case factory and implement get photos use case.

---
+++ PhotoPreview/Domain/UseCases/UseCaseFactory.swift
@@ -0,0 +1,28 @@
+//
+//  UseCaseFactory.swift
+//  PhotoPreview
+//
+//  Created by Tomasz Pietrowski on 27.09.2018.
+//  Copyright © 2018 Tomasz Pietrowski. All rights reserved.
+//
+
+protocol UseCaseProducing: PhotoUseCaseProducing { }
+
+protocol PhotoUseCaseProducing {
+    func getPhotos(count: Int) -> UseCase<[PhotoData]>
+}
+
+final class UseCaseFactory: UseCaseProducing {
+    
+    private let photoGateway: PhotoGateway
+    
+    init(photoGateway: PhotoGateway) {
+        self.photoGateway = photoGateway
+    }
+    
+    func getPhotos(count: Int) -> UseCase<[PhotoData]> {
+        return UseCase { [photoGateway] in
+            photoGateway.getPhotos(count: count)
+        }
+    }
+}
